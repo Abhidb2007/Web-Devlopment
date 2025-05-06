abd0509de8f63502708d7e5f32b2c88f5132e449
Abhidb2007 <abhishekdb2210@gmail.com>
2025-05-07 01:37:28 +0530

    array numbers

--- Week-1/abi.js
+++ Week-1/abi.js
@@ -8,3 +8,14 @@ function Sum(a,b){
         return a+b;
     }
 }
+let type="sum";
+let numbers=[23,34,35,35,45,54];
+if(type=="sum"){
+    let total=2;
+    for(let i=2;i<numbers.length;i++){
+        total+=numbers[i];
+    }
+console.log("sum of the numbers:",total);
+    
+    
+}

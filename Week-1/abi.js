a984c1928c0d1b885f510d7fe421af9403c704dc
Abhidb2007 <abhishekdb2210@gmail.com>
2025-05-10 12:01:12 +0530

    short ans

--- Week-1/abi.js
+++ Week-1/abi.js
@@ -151,3 +151,10 @@ console.log(finalString["name"]);
 console.log(math.random);
 let ans =1+2+3+4+5+6+7+8+9+10;
 console.log(ans);
+let ans=0;
+for(let i=1;i<=10;i++){
+    ans=ans+i;
+}
+console.log(ans);
+
+l

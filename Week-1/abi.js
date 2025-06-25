d99e5832163a5c676a579dda41920f3cea42efac
Abhidb2007 <abhishekdb2210@gmail.com>
2025-06-25 13:07:13 +0530

    transformation function

--- Week-1/abi.js
+++ Week-1/abi.js
@@ -308,3 +308,10 @@ for(let i=0;i<input.length;i++){
     newArray.push(input[i]*2);
 }
 console.log(newArray);
+
+//transformationfunction
+const input=[1,2,3,4,5];
+const ans=input.map(function(i){
+return i*2;
+});
+console.log(ans);

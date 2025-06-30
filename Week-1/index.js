8e0ae1e48e9ff638ea02e8fe7e0fed0777b33314
Abhidb2007 <abhishekdb2210@gmail.com>
2025-06-30 22:25:23 +0530

    loop

--- Week-1/index.js
+++ Week-1/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
-const { clearScreenDown } = require("readline");
 const app = express();
+function sum(n){
+let ans=0;
+for(let i=0;i<=n;i++){
+    ans=ans+i;
+}
+return ans;
+}
 app.get("/",function(req,res){
     res.send("hi there")
-})
+});

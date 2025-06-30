cbff8e542b49049e4cd2b3ed1ed76cadeb482eb2
Abhidb2007 <abhishekdb2210@gmail.com>
2025-06-30 23:15:35 +0530

    code

--- Week-1/index.js
+++ Week-1/index.js
@@ -1,14 +1,6 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-function sum(n){
-let ans=0;
-for(let i=0;i<=n;i++){
-    ans=ans+i;
-}
-return ans;
-}
 app.get("/",function(req,res){
-    const n = req.query.n;
-    const ans= sum(n);
-    res.send("hi there")
-});
+    console.log(1/0);
+    throw new Error(asdasdasd);
+})

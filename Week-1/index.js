52db5e64344793079d9527e449f810da744117fc
Abhidb2007 <abhishekdb2210@gmail.com>
2025-06-30 19:20:40 +0530

    addcode

--- Week-1/index.js
+++ Week-1/index.js
@@ -1,14 +1,1 @@
 const express = require('express');
-const app = express();
-app.post("health check", function (req, res) {
-    const kindneys = req.body.kidneys;
-    const liver = req.body.liver;
-    const heart = req.body.heart;
-    res.send("your heart is good and your liver is good and your kidneys are good");
-});
-
-app.use((res.req.error.next)=>{
-    //console.error("Error occurred:", error);
-    res.status(500).send("Internal Server Error");
-});
-app.listen(3000);
